ee92bd7c8391c5f04f1bfe07885be6c6ad73c08b
Markus Engsner <markus.engsner@gmail.com>
2020-07-31 22:10:16 +0200

    Working IndigoSandbox (fixed trait type, last commit non-working)

--- src/main/scala/stratship/ShipGame.scala
+++ src/main/scala/stratship/ShipGame.scala
@@ -6,7 +6,7 @@ import scala.scalajs.js.annotation.JSExportTopLevel
 import stratship.core.{Model, StartupData, ViewModel}
 
 @JSExportTopLevel("IndigoGame")
-object ShipGame extends IndigoDemo[StartupData, Model] {
+object ShipGame extends IndigoSandbox[StartupData, Model] {
 
   val config: indigo.GameConfig = GameConfig.default.withClearColor(ClearColor.Green)
 
